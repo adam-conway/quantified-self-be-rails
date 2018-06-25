2d2f171a8bce3bb692c5cd108060bc47a7ce9097
adam-conway <adam.n.conway@gmail.com>
2018-06-25 14:12:19 -0600

    Added model test for food

---
+++ spec/models/food_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Food, type: :model do
+  describe 'Field validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:integer) }
+  end
+
+  # describe 'relationships' do
+  # end
+end
