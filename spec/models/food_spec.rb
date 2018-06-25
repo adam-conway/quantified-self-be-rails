d55f4e42bd1394c8243633d72fc54bcc946f6c66
adam-conway <adam.n.conway@gmail.com>
2018-06-25 14:15:25 -0600

    Added test for food model/table and migration/model implementation files

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Food, type: :model do
   describe 'Field validations' do
     it { should validate_presence_of(:name) }
-    it { should validate_presence_of(:integer) }
+    it { should validate_presence_of(:calories) }
   end
 
   # describe 'relationships' do
