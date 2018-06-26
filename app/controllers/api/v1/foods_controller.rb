24ecd2682377b2d3d2773b41fb58668ba2533e2e
adam-conway <adam.n.conway@gmail.com>
2018-06-26 16:58:35 -0600

    Added strong params

--- app/controllers/api/v1/foods_controller.rb
+++ app/controllers/api/v1/foods_controller.rb
@@ -6,7 +6,13 @@ class Api::V1::FoodsController < ApplicationController
   end
 
   def create
-    food = Food.create(name: params[:food][:name], calories: params[:food][:calories].to_i)
+    food = Food.create(food_params)
     render json: food
   end
+
+  private
+    def food_params
+      params.require(:food).permit(:name, :calories)
+    end
+
 end
