6b9427a6cf4ac6faa97c69326cd860299783333a
adam-conway <adam.n.conway@gmail.com>
2018-06-25 18:06:52 -0600

    Added ability to post new foods and disable forgery check for post

--- spec/requests/api/v1/foods_spec.rb
+++ spec/requests/api/v1/foods_spec.rb
@@ -4,9 +4,16 @@ describe "Foods API" do
   it "sends a list of foods" do
     food_list = create_list(:food, 5)
     get '/api/v1/foods'
-    expect(response).to be_success
+    expect(response.successful?)
     foods = JSON.parse(response.body)
 
     expect(foods.count).to eq(food_list.count)
+  end
+
+  it "creates a new of food" do
+    params = {food: {:name => "Test", :calories => 1500}}
+    post "/api/v1/foods", params: params, headers: headers
+    expect(response.successful?)
+    foods = JSON.parse(response.body)
   end
 end
